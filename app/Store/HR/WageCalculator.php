1d8e069ad39c13a62e701c33fa7641c6719f3422
guicazaroto <gui.cazaroto@gmail.com>
2019-12-03 14:35:52 -0300

    [feature] wage calc

--- app/Store/HR/WageCalculator.php
+++ app/Store/HR/WageCalculator.php
@@ -1,10 +1,28 @@
 <?php
 namespace App\Store\HR;
 class WageCalculator {
-  public function makeCalc($developer){
-    if($developer->getWage() < 3000) {
+
+  public function makeCalc($employee){
+    if($employee->getJobRole() == 1) {
+      return $this->calcDevWage($employee);
+    }
+    if ($employee->getJobRole() == 2) {
+      return $this->calcDBAWage($employee);
+    }
+  }
+
+  public function calcDevWage ($dev) {
+    if($dev->getWage() < 3000) {
       return 1350.0;
     } 
       return 3200.0;
   }
+
+  public function calcDBAWage ($dba) {
+    if($dba->getWage() > 2500) {
+      return 2100;
+    } 
+      return 1955;
+  }
+
 }
--- tests/Store/WageCalculatorTest.php
+++ tests/Store/WageCalculatorTest.php
@@ -7,18 +7,32 @@ use App\Store\HR\WageCalculator;
 use PHPUnit\Framework\TestCase;
 
 class WageCalculatorTest extends TestCase {
-  public function testDeveloperWithLessThan3k () {
+  public function testDeveloperWithWageLessThan3k () {
     $calculator = new WageCalculator();
     $developer = new Employee("Lucas", 1500.0, JobRole::DEVELOPER);
     $wage = $calculator->makeCalc($developer);
 
     $this->assertEquals(bcmul(1500.0, 0.9), $wage);
   }
-  public function testDeveloperWithBiggerThen3k () {
+  public function testDeveloperWithWageBiggerThen3k () {
     $calculator = new WageCalculator();
     $developer = new Employee("Jonas", 4000.0, JobRole::DEVELOPER);
     $wage = $calculator->makeCalc($developer);
 
     $this->assertEquals(bcmul(4000.0, 0.8), $wage);
+  }
+  public function testDBAWithWageLessThan2_5k() {
+    $calculator = new WageCalculator();
+    $dba = new Employee("Miguel", 2300.0, JobRole::DBA);
+    $wage = $calculator->makeCalc($dba);
+
+    $this->assertEquals(bcmul(2300, 0.85), $wage);
+  }
+  public function testDBAWithWageBiggerThan2_5k() {
+    $calculator = new WageCalculator();
+    $dba = new Employee("Fernando", 2800.0, JobRole::DBA);
+    $wage = $calculator->makeCalc($dba);
+
+    $this->assertEquals(bcmul(2800, 0.75), $wage);
   }
 }
